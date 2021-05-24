1245d044c702e6f5734d99713c595742c275daf4
kelver <kelverwt@gmail.com>
2021-05-24 12:43:54 -0300

    criado background

---
+++ script.js
@@ -0,0 +1,23 @@
+let canvas = document.getElementById('snake');
+let contex = canvas.getContext('2d');
+let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
+
+function criarBG() {
+    contex.fillStyle = 'lightgreen';
+    contex.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function criarCobrinha() {
+    for (i=0; i < snake.length; i++){
+        contex.fillStyle = 'green';
+        contex.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+criarBG();
+criarCobrinha();
